7d937340c28ce7dcc10b955967c24da12df060ef
Gabriela Leal <gabrielamiranda@alu.ufc.br>
2022-05-30 12:31:02 -0300

    feat: login user

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,9 +6,10 @@ import userCreateController from "../controllers/users/userCreate.controller";
 /*
 import userDeleteSelfController from "../controllers/users/userDeleteSelf.controller";
 
-import userLoginController from "../controllers/users/userLogin.controller";
+
 import userUpdateController from "../controllers/users/userUpdate.controller";
 */
+import userLoginController from "../controllers/users/userLogin.controller";
 import userListController from "../controllers/users/userList.controller";
 import userListOneController from "../controllers/users/userListOne.controller";
 import { authUser } from "../middleware/authUser.middleware";
@@ -17,7 +18,7 @@ routes.get("/users", userListController);
 routes.get("/users/:id", userListOneController);
 
 routes.post("/users", userCreateController);
-//routes.post("/users/login", userLoginController);
+routes.post("/users/login", userLoginController);
 
 //routes.patch("/users/:id", authUser, userUpdateController);
 
